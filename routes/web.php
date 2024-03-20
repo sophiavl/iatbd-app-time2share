594e85c4241bc93bb456126f16261c6e5a06053d
sophiavl <vanlieshoutsophia@gmail.com>
2024-03-20 13:36:00 +0100

    first commit

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/products', function () {
+    return view('products.index');
+});
+
+Route::get('/products/product', function () {
+    return view('products.product');
+});
