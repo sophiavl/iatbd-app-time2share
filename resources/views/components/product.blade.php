af729611f1f88200a1e7cc8896e6d995b72e8469
sophiavl <102966872+sophiavl@users.noreply.github.com>
2024-06-09 17:41:22 +0200

    made code in product component DRY

--- resources/views/components/product.blade.php
+++ resources/views/components/product.blade.php
@@ -1,48 +1,16 @@
 @props(['size'=> 'medium', 'title' => 'Title', 'category' => 'Category', 'photo' => 'images\tent.jpg'])
 
-@php
-$imageclass = "object-cover ";
-$h1class = 'font-medium ';
-$h3class = '';
-$clockclass = 'text-accent ';
-$pclass = ' ';
-
-
-if($size === "small"){
-    $imageclass .= 'w-32';
-    $h1class .= 'text-xs';
-    $h3class .= 'text-xs';
-    $clockclass .= 'w-4 h-4';
-    $pclass .= 'text-xs';
-}
-if($size === "medium"){
-    $imageclass .= 'w-44';
-    $h1class .= 'text-base ';
-    $h3class .= 'text-xs';
-    $clockclass .= 'w-5 h-5';
-    $pclass .= 'text-xs';
-}
-
-if($size === "large"){
-    $imageclass .= 'w-52';
-    $h1class .= 'text-lg';
-    $h3class .= 'text-base';
-    $clockclass .= 'w-6 h-6';
-    $pclass .= ' text-base';
-
-}
-@endphp
 
 <div class="flex flex-col justify-center">
-    <img src="{{ asset($photo) }}" class="{{ $imageclass}}" >
-    <section class="flex justify-center items-center p-2 mt-1">
+    <img src="{{ asset($photo) }}" class="object-cover sm:w-32 md:w-40 lg:w-48" >
+    <section class="flex justify-center items-center p-2 mt-1 max-h-20">
         <section class="flex flex-col w-3/4">
-            <h1 class="{{ $h1class }}">{{ $title }}</h1>
-            <h3 class="{{ $h3class }}">{{ $category }}</h3>  
+            <h1 class="font-medium sm:text-xs md:text-sm lg:text-base">{{ $title }}</h1>
+            <h3 class=" sm:text-sm md:text-base lg:text-lg">{{ $category }}</h3>  
         </section>
-        <section class="flex flex-col items-center w-1/4 justify-center">
-            <x-feathericon-clock class="{{ $clockclass }}"></x-feathericon-clock>
-            <p class="{{ $pclass }}">3 days</p>
+        <section class="flex flex-col items-center w-1/4  justify-center">
+            <x-feathericon-clock class="text-accent sm:w-6 h-6 md:w-7 h-7 lg:w-8 h-8"></x-feathericon-clock>
+            <p class="sm:text-lg md:text-xl lg:text-2xl">3 days</p>
         </section>
     </section>
 </div>
