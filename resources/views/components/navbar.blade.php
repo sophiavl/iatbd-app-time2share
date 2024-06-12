48b49d11c9ebeec09c3baf6f50d014e1bb2a42dc
sophiavl <102966872+sophiavl@users.noreply.github.com>
2024-06-12 13:53:44 +0200

    navbar responsive

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,21 +1,20 @@
 
 <nav class="h-1/5 w-full flex flex-col bg-bgcolor flex">
     <ul class="flex justify-between items-center">
-        <li  class="pl-6"><button id="toggleMenu"><x-feathericon-menu class="w-8 h-8 md:w-10 md:h-10"></x-feathericon-menu></button></li>
+        <li  class="pl-6"><button id="toggleMenu"><x-feathericon-menu class="w-7 h-7 2xl:w-10 2xl:h-10"></x-feathericon-menu></button></li>
         <li><a href="{{ route('start')}}"><x-logosmall></x-logosmall></a></li>
-        <li class="pr-6"><button><a href= "{{ route('profile')}}"><x-feathericon-user class="w-8 h-8 md:w-10 md:h-10"></x-feathericon-user></a></button></li>
+        <li class="pr-6"><button><a href= "{{ route('profile')}}"><x-feathericon-user class="w-7 h-7 2xl:w-10 2xl:h-10"></x-feathericon-user></a></button></li>
     </ul>
-    <div class="w-screen border border-text" style="--border-width: 1px;"; ></div>    
+    <div class="w-full border border-text" style="--border-width: 1px;"; ></div>    
 </nav>
 
     
 <section id="mobile-menu" class="absolute bg-bgcolor shadow-lg w-64 top-24 inset-0 z-50 flex flex-col items-beginning">
-    <div class="w-screen border border-text" style="--border-width: 1px;"></div>    
     <ul class="m-4 flex flex-col">
-        <li class="p-2"><button><a href="{{ route('products.index')}}">Alle producten</a></button></li>
-        <li class="p-2"><button><a href="{{ route('loaned')}}">Uitgeleende producten</a></button></li>
-        <li class="p-2"><button><a href="{{ route('borrowed')}}">Geleende producten</a></button></li>
-        <li class="p-2"><button><a href="{{ route('profile')}}">Profiel</a></button></li>
+        <li class="p-2"><button><a class="md:text-lg" href="{{ route('products.index')}}">Alle producten</a></button></li>
+        <li class="p-2"><button><a class="md:text-lg" href="{{ route('loaned')}}">Uitgeleende producten</a></button></li>
+        <li class="p-2"><button><a class="md:text-lg" href="{{ route('borrowed')}}">Geleende producten</a></button></li>
+        <li class="p-2"><button><a class="md:text-lg" href="{{ route('profile')}}">Profiel</a></button></li>
     </ul>
 </section>
  
